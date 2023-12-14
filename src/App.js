2a0f1753a1f11e2ccbc2a5bb3fec9d9a162dadaa
Stephen Tersoo <stephentersoo17@gmail.com>
2023-12-14 16:50:15 +0100

    Set up react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 
 function App() {
     return (
-        <div className="App">
-            <h1>SPEC SHOPPING MALL</h1>
-        </div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+        </Routes>
     );
 }
 
