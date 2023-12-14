129d9adad7c65c5756b54a92e26af4026186a86f
Stephen Tersoo <stephentersoo17@gmail.com>
2023-12-14 17:15:32 +0100

    mapped through product object data

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { PRODUCTS } from "../data/items";
 
 function Home() {
     return (
         <div className="App">
             <h1>SPEC SHOPPING MALL</h1>
+            <div className="products">
+                {PRODUCTS.map((item) => {
+                    return (
+                        <>
+                            <img src={item.url} />
+                            <div className="title">{item.title}</div>
+                            <div className="add-to-cart">Add to cart</div>
+                        </>
+                    );
+                })}
+            </div>
         </div>
     );
 }
